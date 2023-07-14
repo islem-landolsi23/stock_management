fe10496847f8e0d3083877d84bd6fda8e9fdec9a
Ons Jabeur <ons.ajbeur@esprit.tn>
2023-07-14 23:13:30 +0100

    modif-ons

--- ms_gestionClient/ms_gestionClient/src/main/java/com/example/ms_gestionClient/service/IClientService.java
+++ ms_gestionClient/ms_gestionClient/src/main/java/com/example/ms_gestionClient/service/IClientService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import com.example.ms_gestionClient.entities.Client;
 
 public interface IClientService {
-	public Client addCandidat(Client c);
-	public List<Client> getCandidats ();
+	public Client addClient(Client c);
+	public List<Client> getClients ();
 	public Client getById(int Id);
 }
