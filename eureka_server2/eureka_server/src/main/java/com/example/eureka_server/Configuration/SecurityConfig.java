f7da5cc44ef2d903f27eb6f7da56c6cc31b760b2
islem landolsi <mohamedislem.landolsi@esprit.tn>
2023-07-14 12:06:48 +0100

    eureka server

---
+++ eureka_server2/eureka_server/src/main/java/com/example/eureka_server/Configuration/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.example.eureka_server.Configuration;
+
+
+//@Configuration
+//@EnableWebSecurity
+public class SecurityConfig {
+//    @Override
+//    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder)
+//            throws Exception
+//    {
+//        authenticationManagerBuilder.inMemoryAuthentication().withUser("eureuka")
+//                .authorities("USER");
+//
+//    }
+
+}
